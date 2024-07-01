9aa85ba43346002a1d1f5aafc0e7e2b354f3e93a
Yash Panjwani <yashmpanjwani@gmail.com>
2024-07-01 18:13:25 +0530

    Add Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'YashPanjwani',
   description: 'Web Developer Portfolio',
+  
 }
 
 export default function RootLayout({ children }) {
